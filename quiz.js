789d61431c361098f0eaa533850b19a76dd7b93c
stephanie orozco <orozcostephanie1@yahoo.com>
2021-10-23 23:15:31 -0500

    commit

---
+++ quiz.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Artificial Programming Intelligence",
+      "American Programming Interface",
+      "Application Programming Interface",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "Hypertext Mark Up Language",
+    options: [
+      "Hypertext Microsoft Language",
+      "Hypertext Mark Up Language",
+      "Hyper Transition Mark Up Language",
+      "Hypertext Mailing Language"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Science Query Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Coding Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is a boolean?",
+    answer: "A True or false data type.",
+    options: [
+      "A variable",
+      "A function",
+      "A True or false data type.",
+      "A & B"
+    ]
+  },
+];
